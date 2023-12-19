b03a38397bb71ad2ed6bb2e1a8ed9b1c828181aa
FatimaAli19 <fatimaalimemon21@gmail.com>
2023-12-19 12:54:19 +0500

    api integrated and routing implemented

--- src/Assets/components/Herosection.js
+++ src/Assets/components/Herosection.js
@@ -1,12 +1,13 @@
 import Carousel from 'react-bootstrap/Carousel';
 import image1 from '../images/Group 51.png';
+import image2 from '../images/Group 52.png';
  
 function HeroSec() {
         return (
                 <>
                  
                 <div className='container-fluid bg-dark p-0'>
-                        <Carousel data-bs-theme="dark" className='bg-dark '>
+                        <Carousel data-bs-theme="dark" className=' left-background right-background'>
                                 <Carousel.Item>
                                         <img
                                                 className="d-block ms-auto "
@@ -14,9 +15,17 @@ function HeroSec() {
                                                 alt="First slide"
                                         />
                                 </Carousel.Item>
+                                <Carousel.Item>
+                                        <img
+                                                className="d-block ms-auto "
+                                                src={image2}
+                                                alt="Second slide"
+                                        />
+                                </Carousel.Item>
                         </Carousel> 
                 </div>
                 </>
         );
 }
+
 export default HeroSec;
